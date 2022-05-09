02100cc368c0c1f92c9063af466ca1721eea14d3
ketjj <jorjolianiket@gmail.com>
2022-05-09 22:52:30 +0200

    milestone 5b complete

--- js/script.js
+++ js/script.js
@@ -12,17 +12,19 @@ const app = new Vue ({
                       date: '10/01/2020 15:30:55',
                       message: 'Hai portato a spasso il cane?',
                       status: 'sent',
-                      menuVisible: false,
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Ricordati di stendere i panni',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 16:15:22',
                       message: 'Tutto fatto!',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   }
               ],
           },
@@ -34,17 +36,20 @@ const app = new Vue ({
                   {
                       date: '20/03/2020 16:30:00',
                       message: 'Ciao come stai?',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '20/03/2020 16:30:55',
                       message: 'Bene grazie! Stasera ci vediamo?',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   },
                   {
                       date: '20/03/2020 16:35:00',
                       message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   }
               ],
           },
@@ -56,17 +61,20 @@ const app = new Vue ({
                   {
                       date: '28/03/2020 10:10:40',
                       message: 'La Marianna va in campagna',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   },
                   {
                       date: '28/03/2020 10:20:10',
                       message: 'Sicuro di non aver sbagliato chat?',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '28/03/2020 16:15:22',
                       message: 'Ah scusa!',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   }
               ],
           },
@@ -78,12 +86,14 @@ const app = new Vue ({
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Lo sai che ha aperto una nuova pizzeria?',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Si, ma preferirei andare al cinema',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   }
               ],
           },
@@ -95,12 +105,13 @@ const app = new Vue ({
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ricordati di chiamare la nonna',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Va bene, stasera la sento',
-                      status: 'received'
+                      status: 'received',menuVisible: false
                   }
               ],
           },
@@ -112,17 +123,20 @@ const app = new Vue ({
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ciao Claudia, hai novità?',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Non ancora',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:51:00',
                       message: 'Nessuna nuova, buona nuova',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   }
               ],
           },
@@ -134,12 +148,14 @@ const app = new Vue ({
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   }
               ],
           },
@@ -151,17 +167,21 @@ const app = new Vue ({
                   {
                       date: '10/01/2020 15:30:55',
                       message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:50:00',
                       message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                      status: 'sent'
+                      status: 'sent',
+                      menuVisible: false
                   },
                   {
                       date: '10/01/2020 15:51:00',
                       message: 'OK!!',
-                      status: 'received'
+                      status: 'received',
+                      menuVisible: false,
+                      
                   }
               ],
       
@@ -189,7 +209,7 @@ const app = new Vue ({
       messageActive: 0,
       inputMessage: '',
       searchFriend: '',
-      invisible: false,
+    //   menuVisible: false,
       indexMessage: 0,
 
 
@@ -249,8 +269,8 @@ const app = new Vue ({
   },
 
   deleteCurrentMessage(index){
-
-    this.profiles[this.messageActive].messages.splice(index, 1);
+        this.profiles[this.messageActive].messages.splice(index, 1);   
+   
     //Devi controllare ----> di cancellare anche l'ultimo elemento
 
   },
@@ -259,7 +279,6 @@ const app = new Vue ({
   getRandomAnswers(min, max){
     return Math.floor(Math.random() * (max - min + 1) + min);
   },
-
 
   },
 
